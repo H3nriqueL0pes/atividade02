1d034a1be4f18af4fbc463a60416c6c7d677373a
H3nriqueL0pes <102737503+H3nriqueL0pes@users.noreply.github.com>
2022-05-05 10:54:01 -0300

    Add files via upload

---
+++ atividade02.js
@@ -0,0 +1,27 @@
+//some dois numeros
+
+function some(a,b){
+    return console.log(a + b)
+}
+
+some(5,6)
+
+//multiplique dois numeros
+
+function multiplique(d,c){
+    return console.log(d*c)
+}
+
+multiplique(5,6)
+
+//media
+
+function media(a,b,c,d){
+
+let res = (a+b+c+d)/4
+
+return console.log(res)
+}
+
+media(5,6,9,9)
+
